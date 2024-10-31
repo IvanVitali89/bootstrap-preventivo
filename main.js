f1c0816d3eb16e6d056a7d6018bcbeba43ae3dda
IvanVitali89 <ertre89@gmail.com>
2024-10-31 15:09:38 +0100

    add eventlistener

---
+++ main.js
@@ -0,0 +1,15 @@
+const myForm = document.getElementById("form");
+const discount = [ "YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+
+
+
+
+
+myForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const tipeOfWork = document.getElementById("typeWork").value;
+    console.log(tipeOfWork);
+    
+
+});
