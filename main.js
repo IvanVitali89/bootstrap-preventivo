9b095eeea11ef823727171c5d9c84952b532edc9
IvanVitali89 <ertre89@gmail.com>
2024-10-31 16:48:42 +0100

    add correction to function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const myForm = document.getElementById("form");
-const discount = [ "YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+const discount = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 
 
@@ -7,10 +7,18 @@ const discount = [ "YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 
 myForm.addEventListener('submit', function (event) {
     event.preventDefault();
-
+    const valueDiscount = document.getElementById("Discount").value;
+    const DiscountBigger = valueDiscount.toUpperCase();
     const tipeOfWork = document.getElementById("typeWork").value;
     console.log(tipeOfWork);
-    
+    console.log(valueDiscount);
+    console.log(discountCode(valueDiscount));
+    console.log(DiscountBigger);
+
+
+    //if ()
+
+
 
 });
 
@@ -18,10 +26,14 @@ myForm.addEventListener('submit', function (event) {
 
 /** confronta la stringa con l'array e mi da conferma di eventuale uguaglianza
  * 
- * @param {string} array1 
+ * @param {string} array1
+ * @returns {boolean} 
  */
-function discountCode(array1){
-    for (i=0; i< discount.length; i++);
-    if (array1===discount[i]);
-    return true 
-}
+function discountCode(array1) {
+    for (i = 0; i < discount.length; i++) {
+        if (array1 === discount[i]) {
+            return true;
+        }
+    } 
+    return false;
+}  
