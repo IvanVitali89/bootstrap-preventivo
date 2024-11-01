d32c632b8a7058de9729057b370884b07b3a4c30
IvanVitali89 <ertre89@gmail.com>
2024-11-01 10:08:05 +0100

    add comment

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-const myForm = document.getElementById("form");
+const myForm = document.getElementById("form"); //prendo il form 
 const discount = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 
-
-
-
-myForm.addEventListener('submit', function (event) {
+myForm.addEventListener('submit', function (event) { //blocco l'aggiornamento automatico della pagina
     event.preventDefault();
-    const valueDiscount = document.getElementById("Discount").value;
-    const DiscountBigger = valueDiscount.toUpperCase();
+    const valueDiscount = document.getElementById("Discount").value; //prendo tutti i valori necessari al calcolo
+    const DiscountBigger = valueDiscount.toUpperCase(); //rendo maiuscolo il codice sconto
     let tipeOfWork = document.getElementById("typeWork").value;
-    let finalPrice=0
-    console.log(tipeOfWork);
+    let finalPrice = 0
+    console.log(tipeOfWork); //check che ho tutti i valori giusti
     console.log(valueDiscount);
     console.log(discountCode(DiscountBigger));
     console.log(DiscountBigger);
 
-
-    if ( tipeOfWork==='1' && discountCode(DiscountBigger)==true){
-        finalPrice = (20.50*10/100)*75;
+    // calcolo il valore
+    if (tipeOfWork === '1' && discountCode(DiscountBigger) == true) {
+        finalPrice = (20.50 * 10 / 100) * 75;
     } else {
-        finalPrice = 20.50*10;
-    } if ( tipeOfWork==='2' && discountCode(DiscountBigger)==true){
-        finalPrice = (15.30*10/100)*75;
+        finalPrice = 20.50 * 10;
+    } if (tipeOfWork === '2' && discountCode(DiscountBigger) == true) {
+        finalPrice = (15.30 * 10 / 100) * 75;
     } else {
-        finalPrice = 15.30*10;
-    } if ( tipeOfWork==='3' && discountCode(DiscountBigger)==true){
-        finalPrice = (33.60*10/100)*75;
+        finalPrice = 15.30 * 10;
+    } if (tipeOfWork === '3' && discountCode(DiscountBigger) == true) {
+        finalPrice = (33.60 * 10 / 100) * 75;
     } else {
-        finalPrice = 33.60*10;
+        finalPrice = 33.60 * 10;
     }
     console.log(finalPrice.toFixed(2));
 
     const stampFinalPrice = document.getElementById("result");
     console.log(stampFinalPrice);
-    stampFinalPrice.innerHTML+=
-    `Prezzo Finale € ${finalPrice.toFixed(2)}`;
-    
+    if (discountCode(DiscountBigger) == true) { //controllo se lo sconto è applicato per il messaggio finale
+        stampFinalPrice.innerHTML +=
+            `Prezzo Finale € ${finalPrice.toFixed(2)}`;
+    } else {
+        stampFinalPrice.innerHTML += `
+           Prezzo Finale € ${finalPrice.toFixed(2)}
+    Il codice promozionale non è valido
+    `;
+    }
+
 
 
 });
@@ -53,6 +57,6 @@ function discountCode(array1) {
         if (array1 === discount[i]) {
             return true;
         }
-    } 
+    }
     return false;
 }  
